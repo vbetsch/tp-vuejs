c25c3a70a48130f283717d21e06c0a114904d6de
vbetsch <vic.betsch@gmail.com>
2024-02-15 13:41:47 +0100

    :sparkles: List and add todos

---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+
+let inputValue = ""
+let todos = reactive([
+  "Réviser les cours de DevOps",
+  "Créer une pipeline CI pour le projet Ruptik",
+  "Revoir les design pattern",
+  "Apprendre Vuejs",
+  "Créer un script bash pour push la visu",
+  "Créer un site sur React"
+])
+
+function clickOnAddTodo() {
+  todos.push(inputValue);
+}
+</script>
+
+<template>
+  <input id="new-todo" type="text" v-model="inputValue">
+  <label for="new-todo">
+    <button @click="clickOnAddTodo">Ajouter</button>
+  </label>
+  <div v-for="todo in todos">
+    {{ todo }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
